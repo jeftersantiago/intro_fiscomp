d4f11cb9b89c9318a1e77ee1f8645d21a306bb89
jeftersantiago <jeftersmares@gmail.com>
2022-08-17 16:53:28 -0300

    template para confeccao dos relatorios

---
+++ relatorio01/tarefa1.f
@@ -0,0 +1,12 @@
+c     Tarefa 01
+c     Calcula área e volume de um tórus a partir de raios dados.
+      write(*,*) "Digite os valores dos raios (interno, externo):"
+      read(*,*) ri, re
+      pi = acos(-1e0)
+
+      aArea = 4.e0 * pi ** 2 * re * ri 
+      aVolume = 2.e0 * pi ** 3 * re * ri
+
+      write(*,*) "Area ~= ", aArea
+      write(*,*) "Volume ~= ", aVolume
+      end
---
+++ relatorio01/tarefa2.f
@@ -0,0 +1,15 @@
+c     Tarefa 02
+c     Dados 3 vetores (u, v, w) calcula o volume do paralelepipedo das arestas
+c     definidas por u, v e w - v.
+
+      real*4 u(3)
+      real*4 v(3)
+      real*4 w(v)
+
+      write(*,*) "Vetor u:"
+      read(*,*) u(1), u(2), u(3)
+
+      end
+      
+      
+
